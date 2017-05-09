6892ea545e28543d31b5f2c6864bdd8f3e6e89ea
ML <lucny@sspu-opava.cz>
2017-05-09 19:48:07 +0200

    Django blog - vkladani a zobrazeni prispevku

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'slug', 'publish','status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
+admin.site.register(Post, PostAdmin)
